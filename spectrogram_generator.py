143c1d1730e7eb3b28e144420e5b177c5e421aab
edmundman <45210014+edmundman@users.noreply.github.com>
2023-12-05 18:15:02 +0000

    Update spectrogram_generator.py

--- spectrogram_generator.py
+++ spectrogram_generator.py
@@ -1,24 +1,22 @@
-
 import os
 import sys
 import librosa
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.pyplot import specgram
+from tqdm import tqdm 
 
-def create_save_spectrogram(song,song_name,folder):
+def create_save_spectrogram(song, song_name, folder):
     plt.interactive(False)
     clip, sample_rate = librosa.load(song, sr=None)
     fig = plt.figure(figsize=[0.72,0.72])
     ax = fig.add_subplot(111)
-    ax.specgram(clip,NFFT=512, Fs=2, Fc=0, noverlap=128, cmap='inferno', mode='psd')
+    ax.specgram(clip, NFFT=512, Fs=2, Fc=0, noverlap=128, cmap='inferno', mode='psd')
     ax.axis('off')
     ax.grid(False)
     ax.set_frame_on(False)
     fig.canvas.draw()
-    size = fig.get_size_inches()*fig.dpi
     buf = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8)
-    buf = buf.reshape(fig.canvas.get_width_height()[::-1]+ (3,))
+    buf = buf.reshape(fig.canvas.get_width_height()[::-1] + (3,))
     buf = np.array(buf)
     plt.close(fig)
     buf = buf.astype(np.float32)/255.
@@ -28,13 +26,24 @@ def create_save_spectrogram(song,song_name,folder):
     plt.imsave(outfile, buf)
 
 def create_spectrogram_from_folder(music_folder, out_folder):
-    for filename in os.listdir(music_folder):
-        if filename.endswith(".wav"):
-            path_to_song = os.path.join(music_folder, filename)
-            song = librosa.load(path_to_song, sr=22050, duration=5.0) # load 5 second slice of the song
-            song_slices_folder = os.path.join(out_folder, os.path.splitext(filename)[0])
+    wav_files = [f for f in os.listdir(music_folder) if f.endswith('.wav')]
+    
+    for filename in tqdm(wav_files, desc='Processing', colour='blue'):
+        path_to_song = os.path.join(music_folder, filename)
+        song = librosa.load(path_to_song, sr=22050, duration=5.0)
+        song_slices_folder = os.path.join(out_folder, os.path.splitext(filename)[0])
 
-            if not os.path.exists(song_slices_folder):
-                os.makedirs(song_slices_folder)
+        if not os.path.exists(song_slices_folder):
+            os.makedirs(song_slices_folder)
 
-c_folder, output_folder)
+        create_save_spectrogram(path_to_song, filename, song_slices_folder)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Usage: python spectrogram_generator.py <path_to_music_folder> <path_to_output_folder>")
+        sys.exit(1)
+
+    music_folder = sys.argv[1]
+    output_folder = sys.argv[2]
+
+    create_spectrogram_from_folder(music_folder, output_folder)
